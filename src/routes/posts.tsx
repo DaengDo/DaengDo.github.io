f5abd0bd594418a6b5e05e349de9adf7ba6498a8
DaengDo <ringyoon567@gmail.com>
2024-09-21 02:58:18 +0900

    포스트 삭제 버튼과 그 주변을 마크업하라

--- src/routes/posts.tsx
+++ src/routes/posts.tsx
@@ -16,13 +16,34 @@ const PostListComponent = () => {
 
       <ul style={{ display: "flex", flexDirection: "column", padding: "0 40px", gap: "10px" }}>
         {posts.map(({ id, title, image, dateByEdit }) => (
-          <li style={{ listStyleType: "none", padding: 0, margin: 0 }} key={id}>
-            <Link style={{ display: "flex", gap: "10px" }} to="/post/$postId" params={{ postId: id.toString() }}>
+          <li style={{ listStyleType: "none", padding: 0, margin: 0, width: "fit-content" }} key={id}>
+            <Link
+              style={{
+                display: "flex",
+                gap: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "rgba(200,200,200, 0.5)",
+              }}
+              to="/post/$postId"
+              params={{ postId: id.toString() }}
+            >
               {image && <img style={{ objectFit: "contain" }} src={image} />}
               <div>
                 <p>{formatDate(dateByEdit)}</p>
                 <p>{title}</p>
               </div>
+
+              <button
+                style={{
+                  padding: 0,
+                  border: "none",
+                  backgroundColor: "transparent",
+                  outline: "1px solid black",
+                }}
+              >
+                삭제
+              </button>
             </Link>
           </li>
         ))}
