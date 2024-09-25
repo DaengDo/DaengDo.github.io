037cc4ec8cd6ed59354a960557f28c49cd6ee638
DaengDo <ringyoon567@gmail.com>
2024-09-25 20:11:17 +0900

    css in js 의존성 추가
    type safe하게 스타일 작성하려고 vanilla extract 설치

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { manifestAssetsPlugin } from "./manifestAssetsPlugin";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
@@ -13,6 +14,7 @@ export default defineConfig({
     // eslint()
     TanStackRouterVite(),
     manifestAssetsPlugin(),
+    vanillaExtractPlugin(),
   ],
   server: {
     host: "0.0.0.0",
